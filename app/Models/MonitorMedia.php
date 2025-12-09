0671c063a1bd3142d75ab22e9bd3dad28c0ffda2
morales-lc <thejeromeee@gmail.com>
2025-12-09 14:14:58 +0800

    removed factories

--- app/Models/MonitorMedia.php
+++ app/Models/MonitorMedia.php
@@ -2,12 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class MonitorMedia extends Model
 {
-    use HasFactory;
 
     protected $table = 'monitor_media';
 
